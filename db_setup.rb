d86adfb58c1fc93c712ddbd191dac27b49f2b744
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-06 16:00:16 -0600

    First slide loading successfully

--- db_setup.rb
+++ db_setup.rb
@@ -1,1 +1,1 @@
-# code for database will go here
+
--- main.rb
+++ main.rb
@@ -1,1 +1,29 @@
-# route handlers will go here
+require "sinatra"
+require 'json'
+require "sqlite3"
+require "pry"
+
+DATABASE = SQLite3::Database.new("slidereel.db")
+DATABASE.results_as_hash = true
+
+require_relative "reel_handler"
+
+get "/:id" do
+  @slides = Reel_Handler.next_slide(:id)
+  slides_hash = @slides.map{ |to_arrays|
+      to_arrays.to_hash
+    }
+    
+    json_slides = slides_hash.to_json
+end
+
+get "/" do
+  @slides = Reel_Handler.all
+  slides_hash = @slides.map{ |to_arrays|
+      to_arrays.to_hash
+    }
+    
+    json_slides = slides_hash.to_json
+    
+    erb :homepage
+end
