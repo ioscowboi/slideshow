478673e7ea81bea7bf45ad788abf5e645a2dd9a0
CharDale Barnes <differentaccounts@hotmail.com>
2015-03-07 17:58:39 -0600

    Updated file structure. Doesn't appear .js file is being read

--- main.rb
+++ main.rb
@@ -9,10 +9,10 @@ DATABASE.results_as_hash = true
 require_relative "reel_handler"
 
 get "/update/:id" do
-  change_num = params[:id].to_i
+  new_slide_num = params[:id].to_i
   @slides = Reel_Handler.all
-  slides_hash = @slides[change_num].to_hash
-  json_slides = slides_hash.to_json
+  next_slide_hash = @slides[new_slide_num].to_hash
+  json_slide_to_display = next_slide_hash.to_json
 end
 
 get "/" do
--- public/playslide.js
+++ public/playslide.js
@@ -1,60 +1,65 @@
-document.getElementById("forward").addEventListener('click', move_forward);
-
-var next_slide = 1;
-
-function move_forward(event) {
-  next_slide += 1;
-  document.getElementById("forward").innerHTML = next_slide;
-}
-
-function edit_prompt(event) {
-  next_slide += 1;
-next_slideshow_param();
-}
-
-function next_slideshow_param() {
-  get_new_slide = new XMLHttpRequest;
-  get_new_slide.open("post", "http://localhost:4567/update/"+next_slide"");
-  get_new_slide.send();
-  get_new_slide.addEventListener("load", function(event){
-	  response =JSON.parse(this.response);
-
-    sendData(get_new_slide);
-  
-  })
-}
-
-function sendData(data) {
-  var XHR = new XMLHttpRequest();
-  var urlEncodedData = "";
-  var urlEncodedDataPairs = [];
-  var name;
-
-  // We turn the data object into an array of URL encoded key value pairs.
-  for(name in data) {
-    urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
-  }
-
-  // We combine the pairs into a single string and replace all encoded spaces to 
-  // the plus character to match the behaviour of the web browser form submit.
-  urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
-
-  // We define what will happen if the data is successfully sent
-  XHR.addEventListener('load', function(event) {
-    alert('Yeah! Data sent and response loaded.');
-  });
-
-  // We define what will happen in case of error
-  XHR.addEventListener('error', function(event) {
-    alert('Oups! Something goes wrong.');
-  });
-
-  // We setup our request
-  XHR.open('POST', "http://localhost:4567/students/edited");
-
-  // We add the required HTTP header to handle a form data POST request
-  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
- 
-  // And finally, We send our data.
-  XHR.send(urlEncodedData);
-}
+alert("This proves the file is being read !");
+// window.onload = function(){
+//   var slide_displayer = document.getElementById("forward").addEventListener("click", move_forward)
+//   // .addEventListener('click', move_forward);
+// //
+//   var next_slide = 1;
+//
+//   function move_forward(event) {
+//     document.getElementById("title").innerHTML = get_a_new_slide;
+//     document.getElementById("body").innerHTML = get_a_new_slide;
+//   }
+//
+//   function get_a_new_slide(event) {
+//     next_slide += 1;
+//     next_slideshow_param();
+//   }
+//
+//   function next_slideshow_param() {
+//     get_new_slide = new XMLHttpRequest;
+//     get_new_slide.open("post", "http://localhost:4567/update/"+next_slide"");
+//     get_new_slide.send();
+//     get_new_slide.addEventListener("load", function(event){
+//       response =JSON.parse(this.response);
+//
+//       sendData(get_new_slide);
+//
+//     })
+//   }
+//
+//   function sendData(data) {
+//     var XHR = new XMLHttpRequest();
+//     var urlEncodedData = "";
+//     var urlEncodedDataPairs = [];
+//     var name;
+//
+//     // We turn the data object into an array of URL encoded key value pairs.
+//     for(name in data) {
+//       urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+//     }
+//
+//     // We combine the pairs into a single string and replace all encoded spaces to
+//     // the plus character to match the behaviour of the web browser form submit.
+//     urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+//
+//     // We define what will happen if the data is successfully sent
+//     XHR.addEventListener('load', function(event) {
+//       alert('Yeah! Data sent and response loaded.');
+//     });
+//
+//     // We define what will happen in case of error
+//     XHR.addEventListener('error', function(event) {
+//       alert('Oups! Something goes wrong.');
+//     });
+//
+//     // We setup our request
+//     XHR.open('POST', "http://localhost:4567/students/edited");
+//
+//     // We add the required HTTP header to handle a form data POST request
+//     XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//
+//     // And finally, We send our data.
+//     XHR.send(urlEncodedData);
+//   }
+//
+// }
